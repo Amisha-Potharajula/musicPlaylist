cc61a8a292d78c988016d8e8ad5852fb6083c02c
amisha <amishapatel.p197@gmail.com>
2024-06-04 15:52:55 +0000

    commit message

---
+++ src/components/MusicItem/styledComponents.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+export const MusicListItem = styled.li`
+  display: flex;
+  justify-content: space-between;
+`
+export const TrackInfoContainer = styled.div`
+  display: flex;
+  align-items: center;
+`
+export const MusicTrackImage = styled.img`
+  height: 90px;
+  width: 140px;
+`
+export const NameGenreContainer = styled.div``
+
+export const Name = styled.p`
+  font-family: 'Roboto';
+  color: #ffffff;
+`
+export const Genre = styled.p`
+  color: #3b82f6;
+  font-weight: 500;
+`
+export const DurationDeleteContainer = styled.div`
+  display: flex;
+`
+export const Duration = styled.p`
+  font-family: 'Roboto';
+  color: #ffffff;
+`
+export const DeleteButton = styled.button`
+  background-color: transparent;
+  cursor: pointer;
+`
